040bb1438044ece82745aeb2cbdcc6f6ab251f9f
Robert Wilson <robwilstocco@gmail.com>
2026-06-03 13:29:59 -0300

    Vendor Ranking finished

--- app/(protected)/dashboards/faturamento/page.tsx
+++ app/(protected)/dashboards/faturamento/page.tsx
@@ -1,16 +1,29 @@
+'use client';
 import Card from '@/components/Ui/Card/Card';
 import styles from './styles.module.css';
 import DashboardGrid from '@/components/Dashboards/DashboardGrid/DashboardGrid';
 import DashboardWidget from '@/components/Dashboards/DashboardWidget/DashboardWidget';
-import Avatar from '@/components/Layout/AppLayout/Header/Avatar/Avatar';
-import toBRL from '@/utils/toBRL';
 import VendorCard from '@/components/Dashboards/VendorCard/VendorCard';
+
+const vendor = (
+  <VendorCard
+    name='HUGO DOS SANTOS GONÇALVES'
+    orders='100'
+    meta='10'
+    participation='34'
+    totalValue={3000450}
+    rank={1}
+  />
+)
+const vendors = new Array(8).fill(vendor);
+const top3 = vendors.slice(0, 3);
+const otherVendors = vendors.slice(3, vendors.length);
 
 export default function Usuarios() {
   return (
     <div className={`${styles.dashboardContainer}`}>
       <DashboardGrid>
-        <DashboardWidget cols={6} rows={4}>
+        <DashboardWidget cols={5} rows={4}>
           <div className={styles.card}>
             <div className={styles.cardHead}>
               <h2 className={styles.rankingTitle}>🏆 Ranking</h2>
@@ -19,97 +32,27 @@ export default function Usuarios() {
             <div className={styles.fixedRank}>
               Destaques do Pódio
               <div className={styles.top3Container}>
-                <VendorCard
-                  name='HUGO DOS SANTOS GONÇALVES'
-                  orders='100'
-                  meta='10'
-                  participation='34'
-                  totalValue={3000450}
-                  rank={1}
-                />
-                <VendorCard
-                  name='GABRIEL DE DEUS NICOLAU'
-                  orders='100'
-                  meta='10'
-                  participation='30'
-                  totalValue={2500450}
-                  rank={2}
-                />
-                <VendorCard
-                  name='Nathan Lucca'
-                  orders='100'
-                  meta='10'
-                  participation='25'
-                  totalValue={2000450}
-                  rank={3}
-                />
+                {top3.map((vendor) => (
+                  vendor
+                ))}
               </div>
             </div>
             <div className={styles.defaultRank}>
-              <VendorCard
-                name='HUGO DOS SANTOS GONÇALVES'
-                orders='100'
-                meta='10'
-                participation='34'
-                totalValue={3000450}
-                rank={4}
-              />
-              <VendorCard
-                name='HUGO DOS SANTOS GONÇALVES'
-                orders='100'
-                meta='10'
-                participation='34'
-                totalValue={3000450}
-                rank={5}
-              />
-              <VendorCard
-                name='HUGO DOS SANTOS GONÇALVES'
-                orders='100'
-                meta='10'
-                participation='34'
-                totalValue={3000450}
-                rank={5}
-              />
-              <VendorCard
-                name='HUGO DOS SANTOS GONÇALVES'
-                orders='100'
-                meta='10'
-                participation='34'
-                totalValue={3000450}
-                rank={5}
-              />
-              <VendorCard
-                name='HUGO DOS SANTOS GONÇALVES'
-                orders='100'
-                meta='10'
-                participation='34'
-                totalValue={3000450}
-                rank={5}
-              />
-              <VendorCard
-                name='HUGO DOS SANTOS GONÇALVES'
-                orders='100'
-                meta='10'
-                participation='34'
-                totalValue={3000450}
-                rank={5}
-              />
-              <VendorCard
-                name='HUGO DOS SANTOS GONÇALVES'
-                orders='100'
-                meta='10'
-                participation='34'
-                totalValue={3000450}
-                rank={5}
-              />
-              <VendorCard
-                name='HUGO DOS SANTOS GONÇALVES'
-                orders='100'
-                meta='10'
-                participation='34'
-                totalValue={3000450}
-                rank={5}
-              />
+              {/* Se o tamanho do Array dos vendedores for menor que 8, não adicionar autoScroll - vai ficar estranho */}
+              <div className={`${vendors.length >= 8 && styles.autoScroll}`}>
+                <div className={styles.vendorGroup}>
+                  {otherVendors.map((vendor) => (
+                    vendor
+                  ))}
+                </div>
+                <div className={styles.vendorGroup} aria-hidden="true">
+                  {vendors.length >= 8 && (
+                    otherVendors.map((vendor) => (
+                      vendor
+                    ))
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </DashboardWidget>
@@ -118,12 +61,12 @@ export default function Usuarios() {
             <p>Gauge</p>
           </Card>
         </DashboardWidget>
-        <DashboardWidget cols={3} rows={2}>
+        <DashboardWidget cols={4} rows={2}>
           <Card>
             <p>Spot</p>
           </Card>
         </DashboardWidget>
-        <DashboardWidget cols={2} rows={1}>
+        <DashboardWidget cols={3} rows={1}>
           <Card>
             <p>Fat. diario</p>
           </Card>
@@ -133,7 +76,7 @@ export default function Usuarios() {
             <p>ritmo meta</p>
           </Card>
         </DashboardWidget>
-        <DashboardWidget cols={2} rows={1}>
+        <DashboardWidget cols={3} rows={1}>
           <Card>
             <p>vol. pedidos</p>
           </Card>
